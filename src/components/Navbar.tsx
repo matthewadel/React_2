1f56fffc739e6ff0bcd8699a70ffd83141df284d
matthewadel <matthewadel69@yahoo.com>
2025-07-08 23:35:14 +0300

    finish navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,34 @@
+import Logo from '../assets/logo.png'
+import { FaBars, FaTimes } from 'react-icons/fa'
+import './Navbar.css'
+import { useState } from 'react'
+
+export const Navbar = () => {
+    const [click, setClick] = useState(false)
+    return (
+        <div className="screen-container">
+            <nav className="navbar">
+                <a href="/" className="logo">
+                    <img src={Logo} alt="logo" />
+                </a>
+                <div className='hamburger' onClick={() => setClick(!click)}>
+                    {click ? <FaTimes size={30} className='icon' /> : <FaBars size={30} className='icon' />}
+                </div>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                    <li className='nav-item'>
+                        <a href="/">Home</a>
+                    </li>
+                    <li className='nav-item'>
+                        <a href="/">About</a>
+                    </li>
+                    <li className='nav-item'>
+                        <a href="/">Testimonials</a>
+                    </li>
+                    <li className='nav-item'>
+                        <a href="/">Demo</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
