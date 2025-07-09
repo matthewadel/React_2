35ad6515bbc2b71bfce441c1f99c6deb23477eb8
matthewadel <matthewadel69@yahoo.com>
2025-07-09 11:53:20 +0300

    finish hero section

---
+++ src/components/Hero.tsx
@@ -0,0 +1,15 @@
+import './Hero.css';
+
+export const Hero = () => {
+    return (
+        <div className="hero">
+            <div className='content'>
+                <p>Call us</p>
+                <p>0100 652 8347</p>
+                <p>Because Money</p>
+                <p>Doesn't come with instructions</p>
+            </div>
+            <button className="button">Free Consultation</button>
+        </div>
+    )
+}
