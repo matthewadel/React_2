10e83ee293764492c16c2f3a0d3e6300d95d04ec
matthewadel <matthewadel69@yahoo.com>
2025-07-09 17:11:21 +0300

    finish footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,30 @@ import './Footer.css'
 
 export const Footer = () => {
     return (
-        <div>Footer</div>
+        <div className='footer'>
+            <div className='container'>
+
+                <ul>
+                    <li className='nav-item'>
+                        <a href="/">Home</a>
+                    </li>
+                    <li className='nav-item'>
+                        <a href="/">About</a>
+                    </li>
+                    <li className='nav-item'>
+                        <a href="/">Testimonials</a>
+                    </li>
+                    <li className='nav-item'>
+                        <a href="/">Demo</a>
+                    </li>
+                </ul>
+
+
+                <span className='line'></span>
+
+                <p>2025 Execute, Inc. All rights reserved</p>
+
+            </div>
+        </div>
     )
 }
